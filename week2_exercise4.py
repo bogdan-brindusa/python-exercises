3c8696ef4766f5583650a2cd8195ebb41f816782
bogdan-brindusa <44159540+bogdan-brindusa@users.noreply.github.com>
2018-11-20 13:01:24 +0000

    Exercise 4 from Week 2
    Using built-in function print (object(s), separator=separator, end=end, file=file, flush=flush)

---
+++ week2_exercise4.py
@@ -0,0 +1,9 @@
+'''
+	Write a program that produces the following output:
+		Hello World
+		I am in my ISD class right now
+'''
+
+# I use print() instruction for this task
+print("Hello world")
+print("I am in my ISD class right now")
