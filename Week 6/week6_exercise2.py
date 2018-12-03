b5aabc5721ab5fed3ce16f17c6d3635c73aaef2b
bogdan-brindusa <44159540+bogdan-brindusa@users.noreply.github.com>
2018-12-03 18:49:45 +0000

    Add files via upload
    Adding source code files for exercises in Week 6

---
+++ Week 6/week6_exercise2.py
@@ -0,0 +1,11 @@
+'''
+	Supply a condition in this if statement to test if the user entered a “Y”:
+	
+		userInput = input("Enter Y to quit.") 
+		if . . . // supply statement 
+			print("Goodbye")    // if the user entered “Y”
+'''
+
+userInput = input("Enter Y to quit.") 
+if userInput == 'Y':	# supply statement 
+	print("Goodbye")	# if the user entered “Y”
